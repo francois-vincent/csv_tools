4f6a5153e7abf274f0dab3f0b26e5f3241ea3532
Nicolas Dlubak <dlubak@adlere.priv>
2018-09-02 16:09:18 +0200

    clean code

--- app/read_csv.py
+++ app/read_csv.py
@@ -2,27 +2,14 @@ class ReadCsv(object):
 
     def __init__(self, file_csv):
 
-        self.list_header, self.list_field = [], []
+        self.list_field = []
 
         f = open(file_csv, 'r')
 
         for c, v in enumerate(f):
             if c == 0:
-                self.list_header.append(v)
+                self.list_header = v.strip().split(';')
             else:
-                self.list_field.append(v)
-           # print(self.list_header)
-        #print(self.list_field)
+                self.list_field.append(v.strip().split(';'))
 
-
-        # for i in enumerate(f):
-        #
-        #     if i.count(0):
-        #         self.list_header.append(i[1])
-        #     else:
-        #         self.list_field.append([i[1]])
-        #
-        #     print(self.list_header)
-        # print(self.list_field)
-       # [ for i in enumerate(f) if i.count(0)  self.list_header.append(i[1])]
         f.close()
